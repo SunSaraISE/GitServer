311e4dbb0bd5d7310d6271cb256583bb7ef8ed6b
SunSEI <samikovei@gmail.com>
2024-01-09 19:11:47 +0700

    что

--- mozg.js
+++ mozg.js
@@ -3,12 +3,12 @@ var i, content, otkroisa;
 otkroisa=document.getElementsByClassName("otkroisa");
 for(i=0; i<otkroisa.length; i++)
 {
-    otkroisa[i].style.display="none";
+    otkroisa[i].style.display='none';
 }
 content = document.getElementsByClassName("content");
 for (i = 0; i < content.length; i++) 
 {
-    content[i].className = content[i].className.replace(" active", "");
+    content[i].className = content[i].className.replace("active", "");
 }
 document.getElementById(charm).style.display = "block";
 event.currentTarget.className += " active";
